4eea49dc08341fed25f9f469754730617a3e80e5
Jack <jackohara96@gmail.com>
2021-08-03 18:36:49 -0400

    Added UI

--- rockpaperscissorss.js
+++ rockpaperscissorss.js
@@ -1,8 +1,7 @@
-//Random computer selection
 function computerRandom() {
-    let gameOp = ["rock", "paper", "scissors"];
-    return gameOp[Math.floor(Math.random()*gameOp.length)];
-    
+  let gameOp = ["Rock", "Paper", "Scissors"];
+  return gameOp[Math.floor(Math.random()*gameOp.length)];
+  
 }
 
 
@@ -11,44 +10,64 @@ let playerSelection;
 let compScore=0;
 let playerScore=0;
 
+//Play again button
+const j=document.getElementById("play-again");
+function reload() {
+  reload = location.reload();
+}
+j.addEventListener("click", reload, false);
+
+
 //One round of game
 function round (playerSelection){
+let f= document.getElementById('results');
+let computerSelection = computerRandom();
+let result="";
+
+
+if (compScore==5) {
   
-  let computerSelection = computerRandom();
-  let result="";
- if ((playerSelection=="rock" && computerSelection=="scissors") || (playerSelection=="scissors" && computerSelection=="paper") || (playerSelection=="paper" && computerSelection=="rock")) {
-  playerScore++;
-  result =(`${playerSelection} beats ${computerSelection}. You win! Player Score= ${playerScore} and Computer Score =${compScore}`);
-    
- } else if ((playerSelection=="rock" && computerSelection=="paper") || (playerSelection=="scissors" && computerSelection=="rock") || (playerSelection=="paper" && computerSelection=="scissors")) {
-   compScore +=1;
-   result= (`${computerSelection} beats ${playerSelection}. You lose! Player Score= ${playerScore} and Computer Score =${compScore}`);
+  result+=(`You lose, try again! Player Score= ${playerScore} and Computer Score =${compScore}`);
+    f.innerHTML=result;
+
+
+  } else if (playerScore==5) {
    
-} else if (playerSelection==computerSelection) {
-  result= `It's a tie! Player Score= ${playerScore} and Computer Score =${compScore}`;
+    result+=(`Congratulations! You win the game! Player Score= ${playerScore} and Computer Score =${compScore}`);
+    f.innerHTML=result;
+
+
+  } else {
+if ((playerSelection=="Rock" && computerSelection=="Scissors") || (playerSelection=="Scissors" && computerSelection=="Paper") || (playerSelection=="Paper" && computerSelection=="Rock")) {
+playerScore++;
+result =(`${playerSelection} beats ${computerSelection}. You win! Player Score= ${playerScore} and Computer Score =${compScore}`);
   
- }
- console.log(result);
+} else if ((playerSelection=="Rock" && computerSelection=="Paper") || (playerSelection=="Scissors" && computerSelection=="Rock") || (playerSelection=="Paper" && computerSelection=="Scissors")) {
+ compScore +=1;
+ result= (`${computerSelection} beats ${playerSelection}. You lose! Player Score= ${playerScore} and Computer Score =${compScore}`);
+ 
+} else if (playerSelection==computerSelection) {
+result= `It's a tie! Player Score= ${playerScore} and Computer Score =${compScore}`;
+
+}}
+
+f.innerHTML=result;
 
 }
 
-//Plays game 5 times 
-function game() {
 
-    console.log(round(prompt("Rock, paper, or scissors?").toLowerCase()));
-    console.log(round(prompt("Rock, paper, or scissors?").toLowerCase()));
-    console.log(round(prompt("Rock, paper, or scissors?").toLowerCase()));
-    console.log(round(prompt("Rock, paper, or scissors?").toLowerCase()));
-    console.log(round(prompt("Rock, paper, or scissors?").toLowerCase()));
-    if (compScore>playerScore) {
-      console.log(`You lose, try again! Player Score= ${playerScore} and Computer Score =${compScore}`)
-    } else if (playerScore>compScore) {
-      console.log(`Congratulations! You win the game! Player Score= ${playerScore} and Computer Score =${compScore}`)
-    } else (
-      console.log(`Game over, it was a tie! Player Score= ${playerScore} and Computer Score =${compScore}`)
-    )
-  return;
-  }
-  
-  //Starts Game
-  game();
+
+//makes buttons 
+
+document.getElementById('Rock').addEventListener("click", function() {
+round('Rock');
+});
+
+document.getElementById('Paper').addEventListener("click", function() {
+round('Paper');
+});
+document.getElementById('Scissors').addEventListener("click", function() {
+round('Scissors');
+});
+
+ 
